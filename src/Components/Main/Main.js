f1121b5aeac27ae2b758817602f51b155f9b0cd0
Samwij <samwij@Samanthas-MBP-2.modem>
2023-09-15 11:14:22 +1000

    component structure created and App.js updated

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MainSection = () => {
+  return (
+    <main className="main">
+      <div className="section">
+        {/* Content for the first section */}
+      </div>
+      <div className="section">
+        {/* Content for the second section */}
+      </div>
+    </main>
+  );
+};
+
+export default MainSection;
---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const NavigationBar = () => {
+  return (
+    <nav className="navbar">
+      {/* Add navigation links here */}
+    </nav>
+  );
+};
+
+export default NavigationBar;
