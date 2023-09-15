32d7244b5d1765a223ba398f08afbe3285000950
Samwij <samwij@Samanthas-MBP-2.modem>
2023-09-15 14:34:37 +1000

    header baseline created

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import headerStyles from './header.module.css'
 
 const Header = () => {
   return (
-    <header className="header">
-      <h1>Your Website Header</h1>
+    <header className={headerStyles.header}>
+      <div class={headerStyles.row}>
+        <div class={headerStyles.column}>
+          <div class={headerStyles.joinCta}>
+            <h1 class={headerStyles.heroTitle}>Start Playing Football Today!</h1>
+            <p class={headerStyles.ctaText}>Join FutureSport and become a part of the Soccer community.</p>
+            <div class={headerStyles.join} ><a href="/join"><button class={headerStyles.joinBtn}>Join!</button></a></div>
+          </div>
+        </div>
+        <div class={headerStyles.column} id="video">
+          <iframe src="https://www.youtube.com/embed/3zkumKSIEhA" title="Sam Kerr Backflip"></iframe>
+        </div>
+      </div>
     </header>
   );
 };
