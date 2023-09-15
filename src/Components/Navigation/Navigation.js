5c3af3d3e8f0004ca2ee23bc731b4289804e7e94
Samwij <samwij@Samanthas-MBP-2.modem>
2023-09-15 13:54:41 +1000

    navigation base styling complete

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,37 +1,25 @@
 import React from 'react';
+import navStyles from './nav.module.css'
 import { NavLink } from "react-router-dom";
 import Logo from '../../Assets/logo.svg';
 
 
 const NavigationBar = () => {
-  const navContainer = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    color: '#fff',
-    backgroundColor: '#051d40',
-    padding: '10px 0',
-    position: 'sticky',
-    top: 0,
-    zIndex: 100,
-  };
-
-
 
   return (
-    <div className={navContainer}>
+    <div className={navStyles.navContainer}>
       <div>
-        <a href="/"><img src={Logo} /> </a>
+        <a className={navStyles.logo} href="/"><img src={Logo} /> </a>
       </div>
-      <nav>
-        <NavLink className="nav-link" activeClassName="is-active" to="/AboutUs"> About </NavLink>
-        <NavLink className="nav-link" activeClassName="is-active" to="/Sponsorship"> Sponsorship </NavLink>
-        <NavLink className="nav-link" activeClassName="is-active" to="/Schools"> School Programs </NavLink>
-        <NavLink className="nav-link" activeClassName="is-active" to="/Fixture"> Fixture </NavLink>
-        <NavLink className="nav-link" activeClassName="is-active" to="/Training"> Training </NavLink>
-        <NavLink className="nav-link" activeClassName="is-active" to="/Fees"> Fees </NavLink>
-        <NavLink className="nav-link" activeClassName="is-active" to="/ContactUs"> Contact Us </NavLink>
-        <NavLink className="navbar-item join" activeClassName="is-active" to="Join"> Join </NavLink>
+      <nav className={navStyles.navLinkContainer}>
+        <NavLink className={navStyles.navLink} to="/AboutUs"> About </NavLink>
+        <NavLink className={navStyles.navLink} to="/Sponsorship"> Sponsorship </NavLink>
+        <NavLink className={navStyles.navLink} to="/Schools"> School Programs </NavLink>
+        <NavLink className={navStyles.navLink} to="/Fixture"> Fixture </NavLink>
+        <NavLink className={navStyles.navLink} to="/Training"> Training </NavLink>
+        <NavLink className={navStyles.navLink} to="/Fees"> Fees </NavLink>
+        <NavLink className={navStyles.navLink} to="/ContactUs"> Contact Us </NavLink>
+        <NavLink className={navStyles.navLink} to="Join"> Join </NavLink>
       </nav>
     </div>
   );
