512c1d53fa8369766d3c2495bac9500b023895c4
Samwij <samwij@Samanthas-MBP-2.modem>
2023-09-15 16:27:14 +1000

    fixture data imported into component

--- src/Components/Main/Fixture.js
+++ src/Components/Main/Fixture.js
@@ -4,10 +4,32 @@ import fixtureData from '../../Data/fixture.json';
 
 console.log(fixtureData);
 
-function Fixture() {
+const Fixture = () => {
   return (
-    <h1>Fixture</h1>
+    <div>
+      <h1>Fixture List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Round</th>
+            <th>Title</th>
+            <th>Location</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fixtureData.fixture.map((fixture, index) => (
+            <tr key={index}>
+              <td>{fixture.round}</td>
+              <td>{fixture.title}</td>
+              <td>{fixture.location}</td>
+              <td>{fixture.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
-}
+};
 
 export default Fixture;
