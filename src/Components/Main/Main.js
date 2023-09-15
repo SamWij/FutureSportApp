f188946c323ef0cf4c0e1ab9561e554255c029fc
Samwij <samwij@Samanthas-MBP-2.modem>
2023-09-15 15:15:49 +1000

    homepage basics created

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import mainStyles from './main.module.css'
+import Gallery from '../PhotoGallery/PhotoGallery';
 
 const Main = () => {
+
+  const sectionContent = {
+    title: 'Football title 1',
+    content: "Victoria boasts a robust local soccer scene, with clubs spread across the state. From metropolitan Melbourne to regional towns, soccer pitches are alive with the sounds of cheering fans and the thud of the ball hitting the back of the net. The state's governing body, Football Victoria, plays a pivotal role in organizing competitions, fostering talent, and promoting the sport at all levels. One of the most remarkable aspects of local soccer in Victoria is its inclusivity.Clubs here often represent a microcosm of the diverse communities that call the state home.Players from various cultural backgrounds unite under a common love for the game, creating a rich tapestry of experiences.This diversity is celebrated through events such as cultural festivals and international tournaments, where different communities showcase their unique styles of play.",
+  }
+
   return (
-    <main className="main">
-      <section>
-        <h1>Section 1</h1>
+    <main className={mainStyles.main}>
+      <section className={mainStyles.content}>
+        <h1>{sectionContent.title}</h1>
+        <p>{sectionContent.content}</p>
       </section>
-      <section>
-        <h1>Section 2</h1>
+      <section className={mainStyles.Gallery}>
+        <Gallery />
       </section>
     </main>
   );
---
+++ src/Components/PhotoGallery/PhotoGallery.js
@@ -0,0 +1,52 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import galleryStyles from './gallery.module.css'
+import image1 from '../../Assets/soccerImage1.png';
+import image2 from '../../Assets/soccerImage2.png';
+import image3 from '../../Assets/soccerImage3.png';
+import image4 from '../../Assets/soccerImage4.png';
+
+function PhotoGallery() {
+
+  const images = [
+    {
+      id: "1",
+      pic: image1,
+      text: "soccer player 1"
+    },
+    {
+      id: "2",
+      pic: image2,
+      text: "soccer player 2"
+    },
+    {
+      id: "3",
+      pic: image3,
+      text: "soccer player 3"
+    },
+    {
+      id: "4",
+      pic: image4,
+      text: "soccer player 4"
+    }
+  ];
+  return (
+    <div class="column" >
+
+      <Carousel
+        className={galleryStyles.slides}
+        autoPlay
+        infiniteLoop
+        centerMode
+        showThumbs={false}
+        interval={5000}
+      >
+        {images.map((image) => (
+          <img src={image.pic} alt={image.text} className={galleryStyles.slideImage} />
+        ))}
+      </Carousel>
+    </div >
+  );
+}
+
+export default PhotoGallery;
