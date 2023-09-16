9bea0d1a6a67c9727969699ddafb7ad938cc4494
Samwij <samwij@Samanthas-MBP-2.modem>
2023-09-16 13:31:39 +1000

    fees complete

--- src/Components/Main/Fees.js
+++ src/Components/Main/Fees.js
@@ -1,18 +1,40 @@
 
 import React from "react";
-import trainingData from '../../Data/fees.json';
-import feesStyles from './fees.module.css'
+import feeData from '../../Data/fees.json';
+import feesStyles from './fees.module.css';
 
-const Fees = () => (
-  <section className={feesStyles.container}>
-    <div className={feesStyles.leftColumn}>
-    </div>
-    <div className={feesStyles.rightColumn}>
-      <iframe src="https://www.youtube.com/embed/3zkumKSIEhA" title="Sam Kerr Backflip"></iframe>
-    </div>
+console.log(feeData);
 
+const Fee = () => {
+  return (
+    <section className={feesStyles.container}>
+      <h1>Fees</h1>
+      <div className={feesStyles.leftColumn}>
+        <table className={feesStyles.fees}>
+          <thead>
+            <tr>
+              <th>Fee Type</th>
+              <th>Indicative Cost</th>
+              <th>Frequency</th>
+            </tr>
+          </thead>
+          <tbody>
+            {feeData.fees.map((fees, index) => (
+              <tr key={index}>
+                <td>{fees.type}</td>
+                <td>{fees.cost}</td>
+                <td>{fees.frequency}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className={feesStyles.rightColumn}>
+        <iframe src="https://www.youtube.com/embed/3zkumKSIEhA" title="Sam Kerr Backflip"></iframe>
+      </div>
+    </section >
 
-  </section>
-);
+  );
+};
 
-export default Fees;
+export default Fee;
